b753f0fbaf84e9d563f56832f098008265cfb929
Dang Nguyen <danghwin@gmail.com>
2020-07-01 22:11:06 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,11 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "SELECT MIN(gpa) FROM students"
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
+  
 end
 
 def average_gpa_for_9th_grade
+  
 end
